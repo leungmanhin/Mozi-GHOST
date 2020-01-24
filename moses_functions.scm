392bb6f58502fc73a2ca86d307b1b22650eace69
leungmanhin <manhinleung@gmail.com>
2020-01-24 14:02:48 +0800

    Fix brackets and identations for moses_functions.scm

--- moses_functions.scm
+++ moses_functions.scm
@@ -1,41 +1,35 @@
 (define-public (find-dataset-result x)
-    (let* (
-        (dataset (multi-word x))
-    )
-    (cog-execute! 
-        (GetLink
-            (VariableNode "$x")
-            (EquivalenceLink
-                (ConceptNode data)
-                (VariableNode "$x")    
+    (let* ((dataset (multi-word x)))
+        (cog-execute!
+            (GetLink
+                (VariableNode "$x")
+                (EquivalenceLink
+                    (ConceptNode data)
+                    (VariableNode "$x")
                 )
-            )        
+            )
         )
+    )
 )
 
 
 (define-public (find-project-result x)
-    (let* (
-        (project (multi-word x))
-    )
-    (cog-execute!
-        (GetLink
-            (VariableNode "$x")
-            (VariableNode "$d")
-            (AndLink
-                (Inheritance
-                    (VariableNode "$d")
-                    (ConceptNode project)
+    (let* ((project (multi-word x)))
+        (cog-execute!
+            (GetLink
+                (VariableNode "$x")
+                (VariableNode "$d")
+                (AndLink
+                    (Inheritance
+                        (VariableNode "$d")
+                        (ConceptNode project)
                     )
-
-                (EquivalenceLink
-                    (ConceptNode data)
-                    (VariableNode "$d")    
-                )
+                    (EquivalenceLink
+                        (ConceptNode data)
+                        (VariableNode "$d")
+                    )
                 )
             )
-        
-        ) 
-        
         )
+    )
 )
